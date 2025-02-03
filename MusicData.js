6bccb948bc3fec15b7a7da1990cfb3339452908e
Tayler <tuq24156@temple.edu>
2025-02-02 21:12:00 -0500

    created classes Musician, Song, Playlist, added constants and getInfo function

--- MusicData.js
+++ MusicData.js
@@ -1,23 +1,43 @@
 //In this file you will declare three classes (object templates)
 
-
 //1. Declare a class called Song, it's constructor should take three parameters: title, artist, album
-
-
+class Song {
+  constructor(title, artist, album) {
+    (this.title = title), (this.artist = artist), (this.album = album);
+  }
+}
 
 //2. Declare a class called Musician, it's constructor should take three parameters: name, instrument, genre
 
-
+class Musician {
+  constructor(name, instrument, genre) {
+    (this.name = name), (this.instrument = instrument), (this.genre = genre);
+  }
+}
 
 //3. Declare a class called Playlist, it's constructor should take four parameters: name, song1, song2, song3, song4
+class Playlist {
+  constructor(name, song1, song2, song3) {
+    (this.name = name),
+      (this.song1 = song1),
+      (this.song2 = song2),
+      (this.song3 = song3);
+  }
+
+  getInfo() {
+    console.log(`The playlist 'Top Down Tuesday' has the following songs:
+    claws by Charli XCX,
+    TEXAS HOLD 'EM by Beyonce, &
+    Oh to Be in Love by Kate Bush.`);
+  }
+}
+
+export { Song, Musician, Playlist };
+
 //4. this class should also define a method called getInfo() that prints text to the console saying
 //  The playlist (whatever name you gave it) has the following songs:
 //         (Song1 name) by (Song1 Artist Name),
 //         (Song2 name) by (Song2 Artist Name),
 //         (Song3 name) by (Song3 Artist Name),
 
-
-
 //5. export all three classes to make them available to the other JS files.
-
-
--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 // 1. Import the all three objects (Song, Musician, Playlist) that you created in the MusicData.js module file
-
+import { Song, Musician, Playlist } from "./MusicData.js";
 
 // 2. declare a new constant and assign to it a new musician object to describe the musician for the first song.
 
-
+const charli = new Musician("Charli XCX", "Voice", "Pop");
 
 // 3. declare a new constant and assign to it a new musician object to describe the musician for the second song.
-
-
-
+const bey = new Musician("Beyonce", "Voice", "Pop, Country");
 
 // 4. declare a new constant and assign to it a new musician object to describe the musician for the third song.
-
+const kate = new Musician("Kate Bush", "Voice", "Pop");
 
 // 5. declare a new constant and assign to it a new Song object to describe the first song
 // (make sure to use the Musician object from step 2 in creating this Song object
-
-
+const claws = new Song("claws", charli, "how i'm feeling now");
 
 // 6. declare a new constant and assign to it a new Song object to describe the second song
 // (make sure to use the Musician object from step 3 in creating this Song object
-
+const texas = new Song("TEXAS HOLD 'EM", bey, "Cowboy Carter");
 
 // 7. declare a new constant and assign to it a new Song object to describe the third song
 // (make sure to use the Musician object from step 4 in creating this Song object
-
+const love = new Song("Oh to Be in Love", kate, "The Kick Inside");
 
 // 8. declare a new constant called myPlaylist and assign to it a new Playlist object and make sure to use the three Song objects
 //from steps 5, 6, & 7 when creating it.
 //It must be called myPlaylist
 //It must be called myPlaylist
-
+const myPlaylist = new Playlist("Top Down Tuesday", claws, texas, love);
 
 //9. call the .getInfo() method on myPlaylist
-
+myPlaylist.getInfo();
 
 //DO NOT EDIT BELOW THIS LINE
 //the code below here will insert the Playlist data into the webpage.
